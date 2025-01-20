05473f81b4182ffefe579d0a9aab9ab89758c002
Nagesh Jayaram <nagesh.test@go.com>
2025-01-20 11:39:55 +0530

    wilddog mod dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NageshJ2014/puppy
 
 go 1.23.5
+
+require github.com/NageshJ2014/wilddog v0.0.0-20250120045245-5c1176954187
--- puppy.go
+++ puppy.go
@@ -1,5 +1,7 @@
 package puppy
 
+import "github.com/NageshJ2014/wilddog"
+
 func Bark() string {
 	return "Woof"
 }
@@ -10,4 +12,12 @@ func Barks() string {
 
 func Welcome(s string) string {
 	return "Hello I am you Favourite" + s
+}
+
+func Wildog_bark() string {
+	return wilddog.When_Grownup(Bark())
+}
+
+func Wildog_barks() string {
+	return wilddog.When_Grownup(Barks())
 }
